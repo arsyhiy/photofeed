e9bd2fefc105b5f4fe401fd2f0e9a35a4f355ad5
arsyhiy <arsyhiy32@gmail.com>
2025-07-23 12:24:14 +0300

    update .gitignore

--- photofeed/app/models.py
+++ photofeed/app/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+import uuid
+
+
+class user(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
 
 class GalleryImage(models.Model):
     image = models.ImageField(upload_to='gallery/')
