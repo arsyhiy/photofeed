741b0614964abbb5c8ceca942369ee334172ad1f
arsyhiy <arsyhiy32@gmail.com>
2025-07-23 15:17:23 +0300

    adding simpliest auth system

--- photofeed/app/views.py
+++ photofeed/app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import GalleryImage
 from .forms import UploadImageForm
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 
 def index(request):
     if request.method == 'POST':
@@ -13,3 +17,9 @@ def index(request):
 
     gallery = GalleryImage.objects.all()
     return render(request, 'gallery_with_upload.html', {'form': form, 'gallery': gallery})
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
--- photofeed/photofeed/settings.py
+++ photofeed/photofeed/settings.py
@@ -126,3 +126,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"  # new
+
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" # new
