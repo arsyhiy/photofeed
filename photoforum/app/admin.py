43b51f602f9f34eed03b7b53e688b23c8bc671cc
arsyhiy <arsyhiy32@gmail.com>
2025-07-23 11:46:13 +0300

    minor update

--- photoforum/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app.models import image, forum_user
-
-
-
-
-admin.site.register(image)
-admin.site.register(forum_user)
--- photoforum/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-  path('index', views.index, name='index'),
-  path('feed/', views.imagelistView.as_view(), name='images'),
-]
-
